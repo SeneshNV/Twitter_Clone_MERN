9c29d46a0b49cbdeba35d1fee6c136fc68dfbae0
Senesh Nagodavithana <seneshnagodavithana@gmail.com>
2024-12-24 16:10:49 +0530

    model

---
+++ backend/models/user.model.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    fullName: { type: String, required: true },
+    password: { type: String, required: true, minLength: 6 },
+    email: { type: String, required: true, unique: true },
+    followers: [{ type: mongoose.Schema.ObjectId, ref: "User", default: [] }],
+    following: [{ type: mongoose.Schema.ObjectId, ref: "User", default: [] }],
+  },
+  {
+    timestamps: true,
+  }
+);
