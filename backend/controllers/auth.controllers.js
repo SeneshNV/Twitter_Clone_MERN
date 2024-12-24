8e1554568f89dd6fd450d86ce2adbe45bb7bfeab
Senesh Nagodavithana <seneshnagodavithana@gmail.com>
2024-12-24 22:31:04 +0530

    s

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,3 +1,5 @@
+import bcrypt from "bcryptjs";
+
 export const signup = async (req, res) => {
   try {
     const { fullname, username, email, password } = req.body;
@@ -5,6 +7,35 @@ export const signup = async (req, res) => {
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     if (!emailRegex.test(email)) {
       return res.status(400).json({ error: "Invalid email formate" });
+    }
+
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ error: "Username is already taken" });
+    }
+
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+      return res.status(400).json({ error: "Email is already taken" });
+    }
+
+    //hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullname,
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      generateTokenAndSetCookie(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({ message: "User created successfully" });
+    } else {
     }
   } catch (error) {}
 };
