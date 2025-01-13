34bfc81c6b6e73d7a5ccc9d6532db994a30ce589
Senesh Nagodavithana <seneshnagodavithana@gmail.com>
2025-01-13 20:57:10 +0530

    ui setup

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,25 @@
+import daisyui from "daisyui";
+import daisyUiThemes from "daisyui/src/theming/themes";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+
+  daisyui: {
+    themes: [
+      "light",
+      "retro",
+      {
+        black: {
+          ...daisyUiThemes["black"],
+          primary: "rgb(29, 155, 240)",
+          secondary: "rgb(24, 24, 24)",
+        },
+      },
+    ],
+  },
+};
